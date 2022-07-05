662bc2ae4f6aed6c2c8d06e6c50803452f0db250
Ming Xu <ming.xu@allride.ai>
2022-07-05 19:11:34 +0800

    refactory: put Player before Judger

--- chapter01/tic_tac_toe.py
+++ chapter01/tic_tac_toe.py
@@ -127,52 +127,6 @@ def get_all_states():
 # all possible board configurations
 all_states = get_all_states()
 
-
-class Judger:
-    # @player1: the player who will move first, its chessman will be 1
-    # @player2: another player with a chessman -1
-    def __init__(self, player1, player2):
-        self.p1 = player1
-        self.p2 = player2
-        self.current_player = None
-        self.p1_symbol = 1
-        self.p2_symbol = -1
-        self.p1.set_symbol(self.p1_symbol)
-        self.p2.set_symbol(self.p2_symbol)
-        self.current_state = State()
-
-    def reset(self):
-        self.p1.reset()
-        self.p2.reset()
-
-    def alternate(self):
-        while True:
-            yield self.p1
-            yield self.p2
-
-    # @print_state: if True, print each board during the game
-    def play(self, print_state=False):
-        alternator = self.alternate()
-        self.reset()
-        current_state = State()
-        self.p1.set_state(current_state)
-        self.p2.set_state(current_state)
-        if print_state:
-            current_state.print_state()
-        while True:
-            player = next(alternator)
-            i, j, symbol = player.act()
-            next_state_hash = current_state.next_state(i, j, symbol).hash()
-            current_state, is_end = all_states[next_state_hash]
-            self.p1.set_state(current_state)
-            self.p2.set_state(current_state)
-            if print_state:
-                current_state.print_state()
-            if is_end:
-                return current_state.winner
-
-
-# AI player
 class Player:
     # @step_size: the step size to update estimations
     # @epsilon: the probability to explore
@@ -253,6 +207,49 @@ class Player:
     def load_policy(self):
         with open('policy_%s.bin' % ('first' if self.symbol == 1 else 'second'), 'rb') as f:
             self.estimations = pickle.load(f)
+
+class Judger:
+    # @player1: the player who will move first, its chessman will be 1
+    # @player2: another player with a chessman -1
+    def __init__(self, player1, player2):
+        self.p1 = player1
+        self.p2 = player2
+        self.current_player = None
+        self.p1_symbol = 1
+        self.p2_symbol = -1
+        self.p1.set_symbol(self.p1_symbol)
+        self.p2.set_symbol(self.p2_symbol)
+        self.current_state = State()
+
+    def reset(self):
+        self.p1.reset()
+        self.p2.reset()
+
+    def alternate(self):
+        while True:
+            yield self.p1
+            yield self.p2
+
+    # @print_state: if True, print each board during the game
+    def play(self, print_state=False):
+        alternator = self.alternate()
+        self.reset()
+        current_state = State()
+        self.p1.set_state(current_state)
+        self.p2.set_state(current_state)
+        if print_state:
+            current_state.print_state()
+        while True:
+            player = next(alternator)
+            i, j, symbol = player.act()
+            next_state_hash = current_state.next_state(i, j, symbol).hash()
+            current_state, is_end = all_states[next_state_hash]
+            self.p1.set_state(current_state)
+            self.p2.set_state(current_state)
+            if print_state:
+                current_state.print_state()
+            if is_end:
+                return current_state.winner
 
 
 # human interface
